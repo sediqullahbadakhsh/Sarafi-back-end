62277fc4115fd899300c20b67ec8a8942c3380eb
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-23 12:34:15 +0430

    create users_controller and methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+    
+    def create
+        @user = User.create!(user_params)
+        if @user.save
+            render json: { status: 201, message: 'User created successfully!' }
+        else
+            render json: { status: 401, message: 'User did not created successfully!'}
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def update
+        @user = User.find_by(params[:id])
+        @user.update(user_params)
+        if @user.save
+            render json: { status: 201, message: 'updated created successfully!' }
+        else
+            render json: { status: 401, message: 'User did not updated successfully!'}
+        end
+    end
+
+    private
+    def user_params
+        params.permit(
+            :first_name,
+            :last_name,
+            :mobile,
+            :email,
+            :address,
+            :photo,
+            :password,
+            :status,
+            :user_roles_id
+        )
+    end
+
+end
---
+++ test/controllers/user_sarafis_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UserSarafisControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
