fb04d526cf98006e43efaf79623f1520b7fcc6e2
samiullahbhadur <bahadursamiullah@gmail.com>
2022-11-24 15:53:07 +0430

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   
     resources :user_roles, only: :index
     resources :users, only: [:index, :create, :show, :update] do
-      get 'displaySarafi', to: 'user_sarafis#userIndex'
-    end
-    resources :sarafis, only: [:index, :create, :show, :update] do
-      resources :user_sarafis, only: [:index, :create]
-    end 
+      resources :user_sarafiss, only: [:index, :create]
+      resources :sarafis, only: [:index, :create, :show, :update] do
+        resources :user_sarafis, only: [:index, :create]
+      end
+end
+    
 end
