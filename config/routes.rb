8a6b6be1953bf6b0c63d73b61ee30c25c3adfb43
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-11-27 22:24:56 +0430

    configur cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
-    resources :user_roles, only: :index
+  # Admin
+  resources :user_roles, only: :index
     resources :users, only: [:index, :create, :show, :update] do
       resources :user_sarafiss, only: [:index, :create]
       resources :sarafis, only: [:index, :create, :show, :update] do
         resources :user_sarafis, only: [:index, :create]
       end
-end
+  end
     
 end
